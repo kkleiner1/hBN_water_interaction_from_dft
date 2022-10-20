2d7e1ed13a6018ab77f8f70293b9e9dd8568c48e
Kevin Kleiner <kevingk2@illinois.edu>
2022-10-20 17:19:28 -0400

    Removed unnecessary comment in run_basis_extrapolation_calculations.sh

--- dft/run_basis_extrapolation_calculations.sh
+++ dft/run_basis_extrapolation_calculations.sh
@@ -7,7 +7,7 @@ ecp=paw
 Nk=1
 Ns=1
 
-for ecut_ev in 400 #500 600 700 800
+for ecut_ev in 400 500 600 700 800
 	do
 		echo "Now starting KS-DFT for ${system} with ${functional}, ${ecp}, ecut = ${ecut_ev} eV, Nk = ${Nk}, and Ns = ${Ns}"
 		calc_dir=${base_dir}/${system}_${functional}_${ecp}_ecut_${ecut_ev}_Nk_${Nk}_Ns_${Ns}
