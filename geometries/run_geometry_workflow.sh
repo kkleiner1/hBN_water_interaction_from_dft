2feac5233512fe52772fdffa88ee93e4dcd5906c
Kevin Kleiner <kevingk2@illinois.edu>
2022-10-23 18:45:16 -0400

    Made the system variable manual in run_geometry_workflow.sh

--- geometries/run_geometry_workflow.sh
+++ geometries/run_geometry_workflow.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-system=$1
+system=hBN
 
-for Ns in 1 2 3 4 5
+for Ns in 6 7 8 9 10
 	do
 		python generate_supercells_pymatgen.py $system $Ns
 		python convert_POSCAR_to_xyz_ase.py $system $Ns
