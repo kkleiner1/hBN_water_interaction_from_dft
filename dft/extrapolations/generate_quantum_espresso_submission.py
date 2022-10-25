872d3ffe6c509e96e32005ca6cb3cb7fb2e2a08e
Kevin Kleiner <kevingk2@illinois.edu>
2022-10-25 10:09:24 -0400

    Fixed parallel QE setup to mirrir Alex's example in generate_quantum_espresso_submission.py

--- dft/extrapolations/generate_quantum_espresso_submission.py
+++ dft/extrapolations/generate_quantum_espresso_submission.py
@@ -5,15 +5,18 @@ import numpy as np
 def generate_submission_script_as_giant_string(base_dir, calc_dir):
     giant_string = f"""#!/bin/bash
 #SBATCH --job-name="{calc_dir}"
-#SBATCH --time=08:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=128
+#SBATCH --ntasks-per-node=128
+#SBATCH --ntasks-per-core=1
 #SBATCH --constraint=rome 
 #SBATCH --partition=ccq
 #SBATCH --output=err_out_files/{calc_dir}.out
 #SBATCH --error=err_out_files/{calc_dir}.err
+#SBATCH --mail-type=fail
+#SBATCH --mail-user=kevingk2@illinois.edu
 
-export OMP_NUM_THREADS=128
+export OMP_NUM_THREADS=1
 ulimit -s unlimited
 
 module purge
@@ -22,9 +25,7 @@ module load quantum_espresso/7.1_nix2_gnu_ompi
 source ~/venv-rocky8-iron-project/bin/activate
 
 cd {base_dir}/{calc_dir}
-start_time=`date +%s`
-srun --threads-per-core=$OMP_NUM_THREADS pw.x < scf.in > scf.out
-echo `Runtime = $(expr `date +%s` - $start_time) sec'"""
+time mpirun -np 128 pw.x -i scf.in > scf.out"""
     return giant_string
 
 
