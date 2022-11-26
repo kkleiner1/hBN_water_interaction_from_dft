8b72faa876dbd56e66b26201b250ba9a47ff69c3
Kevin Kleiner <kevingk2@illinois.edu>
2022-11-26 00:49:38 -0500

    Fixed generate_scf_input_file.py in extrapolations/ to allow for lda, pbe, and blyp ECP files, etc.

--- dft/extrapolations/generate_scf_input_file.py
+++ dft/extrapolations/generate_scf_input_file.py
@@ -52,8 +52,25 @@ def extract_geometry_info(system, Ns):
     return num_atoms, supercell_lattice_vectors, atomic_positions
 
 
+def generate_atomic_species_info(
+    functional, masses={"B": 10.811, "N": 14.0067, "O": 15.999, "H": 1.00784}
+):
+    atomic_species_info = ""
+    for atom in masses:
+        if "lda" in functional:
+            pseudopotential_file = f"{atom}.pz-hgh.UPF"
+        elif "pbe" in functional:
+            pseudopotential_file = f"{atom}.pbe-hgh.UPF"
+        elif "lyp" in functional:
+            pseudopotential_file = f"{atom}.blyp-hgh.UPF"
+        atomic_species_info += f"{atom} {masses[atom]} {pseudopotential_file}"
+        if atom != list(masses.keys())[-1]:
+            atomic_species_info += "\n"
+    return atomic_species_info
+
+
 def generate_scf_input_as_giant_string(
-    system, ecut_ryd, Nk, Ns, spin_deg=2, max_val_elec_per_atom=6
+    system, functional, vdw_corr, ecut_ryd, Nk, Ns, spin_deg=2, max_val_elec_per_atom=6
 ):
     num_atoms, supercell_lattice_vectors, atomic_positions = extract_geometry_info(
         system, Ns
@@ -62,6 +79,7 @@ def generate_scf_input_as_giant_string(
         np.unique(np.array([pos[0] for pos in atomic_positions.splitlines()]))
     )
     num_bands = int(max_val_elec_per_atom * num_atoms / spin_deg)
+    atomic_species_info = generate_atomic_species_info(functional)
     giant_string = f"""&control
  calculation='scf',
  restart_mode='from_scratch',
@@ -74,6 +92,8 @@ def generate_scf_input_as_giant_string(
  nat={num_atoms},
  ntyp={num_types},
  ecutwfc={ecut_ryd},
+ input_dft='{functional}',
+ vdw_corr='{vdw_corr}',
  occupations='fixed',
  nbnd={num_bands},
 /
@@ -82,10 +102,7 @@ def generate_scf_input_as_giant_string(
 CELL_PARAMETERS angstrom
 {supercell_lattice_vectors}
 ATOMIC_SPECIES
-B 10.811 B.pbe-n-kjpaw_psl.1.0.0.UPF
-N 14.0067 N.pbe-n-kjpaw_psl.1.0.0.UPF
-O 15.999 O.pbe-n-kjpaw_psl.1.0.0.UPF
-H 1.00784 H.pbe-kjpaw_psl.1.0.0.UPF
+{atomic_species_info}
 ATOMIC_POSITIONS angstrom
 {atomic_positions}
 K_POINTS automatic
@@ -96,11 +113,13 @@ K_POINTS automatic
 base_dir = str(sys.argv[1])
 calc_dir = str(sys.argv[2])
 system = str(sys.argv[3])
-ecut_ryd = float(sys.argv[4])
-Nk = int(sys.argv[5])
-Ns = int(sys.argv[6])
+functional = str(sys.argv[4])
+vdw_corr = str(sys.argv[5])
+ecut_ryd = float(sys.argv[6])
+Nk = int(sys.argv[7])
+Ns = int(sys.argv[8])
 
 with open(f"{base_dir}/{calc_dir}/scf.in", "w") as scf_input_script:
     sys.stdout = scf_input_script
-    giant_string = generate_scf_input_as_giant_string(system, ecut_ryd, Nk, Ns)
+    giant_string = generate_scf_input_as_giant_string(system, functional, vdw_corr, ecut_ryd, Nk, Ns)
     print(giant_string)
